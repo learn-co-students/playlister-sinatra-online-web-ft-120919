be1f3e6c75c0fc4de0b0ec2d4cc23e37900732fb
github username <meliajm@gmail.com>
2020-02-15 14:04:36 -0800

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
-    has_many :artist
-    has_many :song_genre
-    has_many :songs, through: :artist
+    has_many :artists, through: :songs
+    has_many :song_genres
+    has_many :songs, through: :song_genres
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
     belongs_to :artist
     has_many :song_genres
-    has_many :genre, through: :artist
+    has_many :genres, through: :song_genres
 
 end
