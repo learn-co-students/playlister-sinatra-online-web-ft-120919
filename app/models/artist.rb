3f7df2c0b66cd687b5b0e8792cf4a2b9f4304cf2
github username <meliajm@gmail.com>
2020-02-16 19:02:16 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,12 @@
 class Artist < ActiveRecord::Base
     has_many :songs
     has_many :genres, through: :songs
+
+    def slug
+        self.name.downcase.gsub(/\s+/, '-')
+    end
+
+    def self.find_by_slug(slug)
+        self.find_by(name: slug.split("-").collect {|string| string.capitalize}.join(", ").tr(',', ''))
+    end
 end
