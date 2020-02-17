db76b7f84093e6986599ae7c685bd5c89ffcdee7
github username <meliajm@gmail.com>
2020-02-16 19:06:11 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
     has_many :song_genres
     has_many :genres, through: :song_genres
 
+    def slug
+        self.name.downcase.gsub(/\s+/, '-')
+    end
+
+    def self.find_by_slug(slug)
+        self.find_by(name: slug.split("-").collect {|string| string.capitalize}.join(", ").tr(',', ''))
+    end
+
 end
